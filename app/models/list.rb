66d1ae32cc634692f50351f4a33799d232225297
EC2 Default User <ec2-user@ip-172-31-27-137.ec2.internal>
2022-04-02 03:15:44 +0000

    2章完了

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,2 @@
 class List < ApplicationRecord
-  t.string 'title'
-  t.string 'body'
-  t.datetime "created_at", precision: 6, null: false
-  t.datetime "updated_at", precision: 6, null: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_023400) do
+ActiveRecord::Schema.define(version: 2022_04_02_030545) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
