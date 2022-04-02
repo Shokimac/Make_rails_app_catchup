c66379384dfa4afe06b83a936c2433b71fc777bf
EC2 Default User <ec2-user@ip-172-31-27-137.ec2.internal>
2022-04-02 02:44:52 +0000

    FirstCommit

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+  t.string 'title'
+  t.string 'body'
+  t.datetime "created_at", precision: 6, null: false
+  t.datetime "updated_at", precision: 6, null: false
+end
